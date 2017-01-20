bbcb197cc7256635f65a5e1179438196fef845f9
Sina Moeini <sina.moeini@gmail.com>
2017-01-19 23:21:10 -0500

    Initial Commit

---
+++ MAPP/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  MAPP
+//
+//  Created by Sina on 1/19/17.
+//  Copyright © 2017 MIT. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
